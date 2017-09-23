c98249b67d2becea1b8760ea085c82701ca13057
Kenta Kodama <kentakodama@gmail.com>
2017-09-23 15:27:56 -0700

    pushing to heroku

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -14,6 +14,7 @@ import Footer from './footer';
 
 const App = () => (
   <div>
+    
     <header className='nav-bar'>
       <Link to="/">Wine With Strangers</Link>
       <Route path="/" component={GreetingContainer} />
